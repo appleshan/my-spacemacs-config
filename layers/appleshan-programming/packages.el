6e728e898480b9590ae2f69b758d13c0cab77a22
appleshan <apple.shan@gmail.com>
2017-11-04 16:04:05 +0800

    add package: symbol-overlay

--- layers/appleshan-programming/packages.el
+++ layers/appleshan-programming/packages.el
@@ -28,6 +28,7 @@
       prodigy
       plantuml-mode
       smart-tab
+      symbol-overlay
       tldr
       vdiff
       workgroups2
@@ -236,6 +237,16 @@
     (add-to-list 'smart-tab-disabled-major-modes 'mu4e-compose-mode)
     (add-to-list 'smart-tab-disabled-major-modes 'erc-mode)
     (add-to-list 'smart-tab-disabled-major-modes 'shell-mode)))
+
+(defun appleshan-programming/init-symbol-overlay ()
+  (use-package symbol-overlay
+    :init (require 'symbol-overlay)
+    :config
+    (global-set-key (kbd "M-i") 'symbol-overlay-put)
+    (global-set-key (kbd "M-n") 'symbol-overlay-switch-forward)
+    (global-set-key (kbd "M-p") 'symbol-overlay-switch-backward)
+    (global-set-key (kbd "<f7>") 'symbol-overlay-mode)
+    (global-set-key (kbd "<f8>") 'symbol-overlay-remove-all)))
 
 (defun appleshan-programming/init-tldr ()
   (use-package tldr
