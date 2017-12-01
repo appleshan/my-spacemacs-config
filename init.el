9889d16466379fa4d8420d613f8e580ad62eb318
appleshan <apple.shan@gmail.com>
2017-12-01 15:47:48 +0800

    doom-one theme 让我看得更清楚

--- init.el
+++ init.el
@@ -248,10 +248,11 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(django
+   dotspacemacs-themes '(;; @see https://emacs-china.org/t/topic/4038/24
+                         ;; doom 的优点: 只突出显示正文和代码，层次感很强，注意正文，代码块名字、begin line字体颜色依次变淡
+                         doom-one
                          spacemacs-dark
                          spacemacs-light
-                         gotham
                          zerodark)
    ;; If non-nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
