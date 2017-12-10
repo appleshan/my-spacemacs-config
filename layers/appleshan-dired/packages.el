d33338821e9bb553ab3d51983b97ae351c3f309e
appleshan <apple.shan@gmail.com>
2017-12-11 00:15:25 +0800

    dired loads the configuration with with-eval-after-load

--- layers/appleshan-dired/packages.el
+++ layers/appleshan-dired/packages.el
@@ -20,44 +20,39 @@
 ;; List of packages to exclude.
 (setq appleshan-dired-excluded-packages '())
 
+;; Directory operations
 (defun appleshan-dired/post-init-dired ()
-  (use-package dired
-    :defer t
-    :config
-    (progn
-      ;; allow dired to be able to delete or copy a whole dir.
-      (setq dired-recursive-copies  'always) ; "always" means no asking
-      (setq dired-recursive-deletes 'top)    ; "top" means ask once for top level directory
+  (with-eval-after-load 'dired
+    ;; allow dired to be able to delete or copy a whole dir.
+    (setq dired-recursive-copies  'always) ; "always" means no asking
+    (setq dired-recursive-deletes 'top)    ; "top" means ask once for top level directory
 
-      ;; @see http://oremacs.com/2015/01/13/dired-options/
-      ;; 传给 ls 的参数:
-      ;; l: Is the only mandatory one.
-      ;; a: Means to list invisible files.
-      ;; G: Don't show group information. These days, when there are more laptops
-      ;;    than people, the group info is rarely useful.
-      ;; h: Human readable sizes, such as M for mebibytes.
-      ;; 1v: Affects the sorting of digits, hopefully in a positive way.
-      ;; u: sort by access time, newest first
-      ;; --group-directories-first: self-explanatory, I like to have the directories
-      ;; on the top, separate from the files.
-      ;;
-      ;; 另外注意：在 dired-sort 中会拼接参数，
-      ;; 所以 "--group-directories-first" 必须写在 "-laGh1v" 前面.
-      (setq dired-listing-switches "--group-directories-first -laGh1vu")
+    ;; Show directory first
+    ;; @see http://oremacs.com/2015/01/13/dired-options/
+    ;; 传给 ls 的参数:
+    ;; l: Is the only mandatory one.
+    ;; a: Means to list invisible files.
+    ;; G: Don't show group information. These days, when there are more laptops
+    ;;    than people, the group info is rarely useful.
+    ;; h: Human readable sizes, such as M for mebibytes.
+    ;; 1v: Affects the sorting of digits, hopefully in a positive way.
+    ;; u: sort by access time, newest first
+    ;; --group-directories-first: self-explanatory, I like to have the directories
+    ;; on the top, separate from the files.
+    ;;
+    ;; 另外注意：在 dired-sort 中会拼接参数，
+    ;; 所以 "--group-directories-first" 必须写在 "-laGh1v" 前面.
+    (setq dired-listing-switches "--group-directories-first -alh1vu")
 
-      (setq directory-free-space-args "-Phk") ;目录空间选项
-      (setq dired-auto-revert-buffer t)
+    (setq directory-free-space-args "-Phk") ;目录空间选项
+    (setq dired-auto-revert-buffer t)
 
-      (setq find-ls-option '("-print0 | xargs -0 ls -ald" . ""))
+    (setq find-ls-option '("-print0 | xargs -0 ls -ald" . ""))
 
-      (add-hook 'dired-mode-hook 'appleshan//dired-hook)
-
-      (define-key dired-mode-map (kbd "I") 'dired-omit-mode)
-      (define-key dired-mode-map (kbd "DEL") 'vinegar/up-directory)
-      (define-key dired-mode-map (kbd "O") 'dired/open-in-external-app)
-      (define-key dired-mode-map (kbd "S") 'dired/rsync)
-      (define-key dired-mode-map (kbd "z") 'dired/get-size)
-    )))
+    (define-key dired-mode-map (kbd "DEL") 'vinegar/up-directory)
+    (define-key dired-mode-map (kbd "O") 'dired/open-in-external-app)
+    (define-key dired-mode-map (kbd "z") 'dired/get-size)
+  ))
 
 ;; @see http://oremacs.com/2015/01/04/dired-nohup/
 (defun appleshan-dired/post-init-dired-x ()
