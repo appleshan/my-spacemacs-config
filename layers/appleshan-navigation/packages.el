270aee66a68e30ea095860fd5a405f0e36b4d3c4
appleshan <apple.shan@gmail.com>
2017-12-11 17:39:00 +0800

    移动workgroups2包
    把workgroups2包从appleshan-programming 移动到appleshan-navigation

--- layers/appleshan-navigation/packages.el
+++ layers/appleshan-navigation/packages.el
@@ -22,6 +22,7 @@
         evil-escape
         ;; perspeen
         treemacs
+        workgroups2
         ))
 
 ;; List of packages to exclude.
@@ -108,6 +109,43 @@
   (with-eval-after-load 'treemacs
     (setq treemacs-show-hidden-files nil)
     ))
+
+(defun appleshan-navigation/init-workgroups2 ()
+  (use-package workgroups2
+  :init (require 'workgroups2)
+  :config
+  (progn
+    (setq wg-session-load-on-start t) ; default: (not (daemonp))
+
+    ;; Change prefix key (before activating WG)
+    (setq wg-prefix-key (kbd "C-c z"))
+
+    ;; Change workgroups session file
+    (setq wg-session-file "~/.emacs.d/.cache/.emacs_workgroups")
+
+    ;; Set your own keyboard shortcuts to reload/save/switch WGs:
+    ;; "s" == "Super" or "Win"-key, "S" == Shift, "C" == Control
+    (global-set-key (kbd "<pause>")     'wg-reload-session)
+    (global-set-key (kbd "C-S-<pause>") 'wg-save-session)
+    (global-set-key (kbd "s-z")         'wg-switch-to-workgroup)
+    (global-set-key (kbd "s-/")         'wg-switch-to-previous-workgroup)
+
+    ;; What to do on Emacs exit / workgroups-mode exit?
+    (setq wg-emacs-exit-save-behavior           'save)      ; Options: 'save 'ask nil
+    (setq wg-workgroups-mode-exit-save-behavior 'save)      ; Options: 'save 'ask nil
+
+    ;; Mode Line changes
+    ;; Display workgroups in Mode Line?
+    (setq wg-mode-line-display-on t)          ; Default: (not (featurep 'powerline))
+    (setq wg-flag-modified t)                 ; Display modified flags as well
+    (setq wg-mode-line-decor-left-brace "["
+          wg-mode-line-decor-right-brace "]"  ; how to surround it
+          wg-mode-line-decor-divider ":")
+
+    (workgroups-mode 1)
+    (spacemacs|hide-lighter workgroups-mode)
+    )
+  ))
 
 ;; Local Variables:
 ;; coding: utf-8
