14fbd578ae487179ab1c762cbc4874133efb905c
appleshan <apple.shan@gmail.com>
2018-01-10 11:41:39 +0800

    恢复 org 的快捷键绑定

--- layers/appleshan-org/packages.el
+++ layers/appleshan-org/packages.el
@@ -75,26 +75,6 @@
 
     (spacemacs|disable-company org-mode)
 
-    ;;;;;;;;;;;;;;;;;;;;
-    ;; Custom Key Bindings
-    ;;;;;;;;;;;;;;;;;;;;
-    (spacemacs/set-leader-keys
-      ;; Go to next org file in org-agenda-files
-      "oC"  'org-cycle-agenda-files
-
-      ;; save
-      ; "ocg" 'org-clock-goto
-      ; "oci" 'org-clock-in
-      "oS"  'org-save-all-org-buffers
-
-      ;; toggle
-      "oTb" 'org-hide-block-toggle-all
-      "oTi" 'org-toggle-inline-images
-      "oTl" 'org-toggle-link-display
-
-      ;; other
-      "ob" 'org-iswitchb)
-
     ;; 防止不小心编辑了省略部分的内容
     (setq org-catch-invisible-edits 'smart)
 
